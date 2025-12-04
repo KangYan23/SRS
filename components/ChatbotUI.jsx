45bf3c9733e370c994347aab76e2808f883d0f9d
KangYan <kangyan23@gmail.com>
2025-12-04 18:11:17 +0800

    CHANGE WORDS

--- components/ChatbotUI.jsx
+++ components/ChatbotUI.jsx
@@ -285,7 +285,7 @@ const ChatbotUI = () => {
                 </button>
 
                 <div className="flex flex-col items-center w-full h-full mt-4">
-                  <h2 className="text-5xl font-handwritten mb-12 tracking-wider">RESULTS</h2>
+                  <h2 className="text-5xl font-handwritten mb-12 tracking-wider">RECOMMEND PROCEDURE</h2>
 
                   <div className="w-full flex flex-row gap-12 h-full px-12">
                     <div className="flex-1 flex flex-col relative border-l-2 border-b-2 border-black/50 p-4">
